e2eaaca0ab2f5c1f7141f1c803330cabd9eaf418
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-06-14 17:16:06 +0100

    Promoting to V1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'boggle_solver',         # How you named your package folder (MyLib)
   packages = ['boggle_solver'],   # Chose the same as "name"
-  version = '0.20',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package for auto-generating the possible combinations of a boggle grid.',   # Give a short description about your library
   long_description=long_description,
@@ -26,11 +26,11 @@ setup(
   install_requires=[            # I get to this in a second
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
